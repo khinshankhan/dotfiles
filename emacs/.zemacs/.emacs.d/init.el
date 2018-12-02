71b29e1ba4765b4fc581d4aef41fe06a50de0f69
Khinshan Khan <khanshan501@gmail.com>
2018-12-02 13:50:22 -0500

    Update init.el

--- emacs/.zemacs/.emacs.d/init.el
+++ emacs/.zemacs/.emacs.d/init.el
@@ -9,4 +9,14 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; turn off functionality of ctr+z
+(global-unset-key (kbd "C-z"))
+
+;; deletes all the whitespace when you hit C-c backspace or delete
+(use-package hungry-delete
+  :ensure t
+  :config
+  (global-set-key (kbd "C-c <backspace>") 'hungry-delete-backward)
+  (global-set-key (kbd "C-c <deletechar>") 'hungry-delete-forward))
+
 (org-babel-load-file (expand-file-name "~/.zemacs/.emacs.d/myinit.org"))
