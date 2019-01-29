92892f8fa11a8e940252921d2384e68dd2d7ded8
Khinshan Khan <khanshan501@gmail.com>
2019-01-29 07:22:39 -0500

    main emacs config

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,28 @@
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (which-key ace-window avy yasnippet-snippets yasnippet ess company-c-headers merlin tuareg company-jedi pip-requirements flycheck company gitignore-mode magit smartparens rainbow-delimiters aggressive-indent counsel ivy dashboard doom-modeline spacemacs-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-preview ((t (:background "#30343C"))))
+ '(company-preview-common ((t (:foreground "#ABB2BF" :background "#30343C"))))
+ '(company-scrollbar-bg ((t (:background "#30343C"))))
+ '(company-scrollbar-fg ((t (:background "#30343C"))))
+ '(company-template-field ((t (:foreground "#282C34" :background "#C678DD"))))
+ '(company-tooltip ((t (:foreground "#ABB2BF" :background "#30343C"))))
+ '(company-tooltip-annotation ((t (:foreground "#ABB2BF" :background "#30343C"))))
+ '(company-tooltip-common ((t (:foreground "#ABB2BF" :background "#30343C"))))
+ '(company-tooltip-common-selection ((t (:foreground "#ABB2BF" :background "#393F49"))))
+ '(company-tooltip-mouse ((t (:background "#30343C"))))
+ '(company-tooltip-selection ((t (:foreground "#ABB2BF" :background "#393F49"))))
+ '(flycheck-error ((t (:underline (:style line :color "#FF5C33")))))
+ '(flycheck-info ((t (:underline (:style line :color "#80FF80")))))
+ '(flycheck-warning ((t (:underline (:style line :color "#FF9933"))))))
