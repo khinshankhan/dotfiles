7fe0058111859758a5808a62cd6463db0d8c231c
Khinshan Khan <khanshan501@gmail.com>
2019-03-21 20:49:09 -0400

    taking care of symlinks

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,4 +2,7 @@
 ;;; Commentary:
 ;; (package-initialize)
 ;;; Code:
+; refer to https://stackoverflow.com/questions/15390178/emacs-and-symbolic-links
+(setq vc-follow-symlinks t)
+; load config org file
 (org-babel-load-file (concat user-emacs-directory "myinit.org"))
