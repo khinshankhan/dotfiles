138be1fd07d5e96ae66529c4fbbe825c26375344
Khinshan Khan <khanshan501@gmail.com>
2019-04-06 20:15:52 -0400

    don't feel like following links anymore

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,5 @@
 ;; refer to https://stackoverflow.com/questions/15390178/emacs-and-symbolic-links
-(setq vc-follow-symlinks t)
+(setq vc-follow-symlinks nil)
 
 ;; load config org file
 (org-babel-load-file (concat user-emacs-directory "myinit.org"))
