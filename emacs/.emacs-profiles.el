5b3612901ed8994f5a505c55e4cd0a21c0c5eaa5
Khinshan Khan <khanshan501@gmail.com>
2019-09-17 19:57:52 -0400

    [chemacs] updated chemacs paths

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,2 +1,9 @@
-(("default" . ((user-emacs-directory . "~/.emacs.d")))
- ("spacemacs" . ((user-emacs-directory . "~/.spacemacs.d"))))
+(("default"   . ((user-emacs-directory . "~/.emacs.d")))
+ ("working"   . ((user-emacs-directory . "~/Projects/github/test")))
+ ("test"      . ((user-emacs-directory . "~/Projects/github/test")))
+
+ ("ign"       . ((user-emacs-directory . "~/Projects/github/lgneous/emacs.d")))
+ ("dreamo"    . ((user-emacs-directory . "~/Projects/github/dreameh/emacs.d")))
+
+ ("spacemacs" . ((user-emacs-directory . "~/other-emacs-configs/spacemacs.d")))
+ )
