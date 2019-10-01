0c74f637c2dec58a5feae12110cdd8f842853862
Khinshan Khan <khanshan501@gmail.com>
2019-09-30 20:36:59 -0400

    new profile paths for chemacs

--- emacs/.emacs-profiles.el
+++ emacs/.emacs-profiles.el
@@ -1,11 +1,12 @@
 (("default"   . ((user-emacs-directory . "~/.emacs.d")))
+ ;; as im working
  ("working"   . ((user-emacs-directory . "~/Projects/github/test")))
  ("test"      . ((user-emacs-directory . "~/Projects/github/test")))
-
+;; friends
  ("ign"       . ((user-emacs-directory . "~/Projects/github/lgneous/emacs.d")))
  ("dreamo"    . ((user-emacs-directory . "~/Projects/github/dreameh/emacs.d")))
-
+;; in the works, but not main
   ("leaf"     . ((user-emacs-directory . "~/other-emacs-configs/leaf-try")))
-
- ("spacemacs" . ((user-emacs-directory . "~/other-emacs-configs/spacemacs.d")))
+;; other ppls, full on "ide"
+ ("spacemacs" . ((user-emacs-directory . "~/nosymlink-emacs-configs/spacemacs.d")))
  )
