907add7428997db074c64af0c40d00d85b37f039
Khinshan Khan <khanshan501@gmail.com>
2019-12-24 18:59:00 -0500

    updated to 28.0.50, working out a few kinks

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,4 +2,5 @@
 (setq vc-follow-symlinks t)
 
 ;; load config org file
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(let ((file-name-handler-alist nil))
+  (org-babel-load-file (concat user-emacs-directory "config.org")))
