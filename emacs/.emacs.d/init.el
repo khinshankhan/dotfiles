a4fe876c1bc03e6a946e3f0ae5654dd1ea00867d
Khinshan Khan <khanshan501@gmail.com>
2020-01-04 19:41:29 -0500

    no longer need to track init.el since config.org utilizes standalone tangle

--- emacs/.emacs.d/init.el
+++
@@ -1,6 +0,0 @@
-;; refer to https://stackoverflow.com/questions/15390178/emacs-and-symbolic-links
-(setq vc-follow-symlinks t)
-
-;; load config org file
-(let ((file-name-handler-alist nil))
-  (org-babel-load-file (concat user-emacs-directory "config.org")))
