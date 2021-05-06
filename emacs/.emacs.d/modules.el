a9dc286da6bf4e11e75b2be41e9dcc8a8fc01f8a
Khinshan Khan <khanshan501@gmail.com>
2021-05-06 14:17:08 -0400

    added modules

--- emacs/.emacs.d/modules.el
+++ emacs/.emacs.d/modules.el
@@ -1,26 +1,36 @@
-;;; modules.el -*- lexical-binding: t; -*-
+;;; modules.el --- -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Choose which modules to enable and how to enable them.
+
+;;; Code:
 
 (require 'core-module)
 
 (load!
  :tools
  lsp
+ git
  :editor
  company
  ;; editorconfig
  ;; expand-region
- ;; flycheck
+ flycheck
  ;; hungry-delete
  ;; multiple-cursors
  ;; parentheses
  ;; project
  zoom
  :lang
+ json
  (python +cython +lsp)
+ (javascript)
  :ui
  theme
  modeline
- dashboard)
+ dashboard
+ (ivy +swiper))
 
 (provide 'modules)
 ;;; modules.el ends here
