f1c8965e5c8761a4d54b66d835a66ae32c5cfc85
Khinshan Khan <khanshan501@gmail.com>
2022-01-30 10:27:58 -0500

    have polybar support multiple monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,7 +4,20 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar main -c ~/.config/polybar/config &
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [[ $primary == $m ]]; then
+        MONITOR=$m TRAY_POS=right polybar main &
+    else
+        MONITOR=$m TRAY_POS=none polybar secondary &
+    fi
+  done
+fi
