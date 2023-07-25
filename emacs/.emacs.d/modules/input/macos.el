47cf2645a08345e61a1dc967e26827032a3b2df9
Khinshan Khan <shan@uptogood.dev>
2023-07-25 12:30:59 -0400

    add delay for frame maximize on mac

--- emacs/.emacs.d/modules/input/macos.el
+++ emacs/.emacs.d/modules/input/macos.el
@@ -5,4 +5,6 @@
   (setq mac-command-modifier 'meta)
   ;; nice to be full screen initially since i dedicate a workspace to emacs on mac anyways
   ;; hitting the green button on mac has issues apparently
-  (toggle-frame-maximized))
+  (do-once-n-sec-after-emacs-startup!
+   0.15
+   (toggle-frame-maximized)))
