295a15a10ff32eeb91fa193fc8d109dece513483
Khinshan Khan <shan@uptogood.dev>
2023-07-30 20:02:50 -0400

    fix which files graphql lsp server is active for

--- emacs/.emacs.d/modules/lang/graphql.el
+++ emacs/.emacs.d/modules/lang/graphql.el
@@ -9,4 +9,12 @@
   :after graphql-mode)
 
 (lsp! graphql-mode
-  (shan--ide-add 'graphql-mode #'hydra-lsp/body))
+  (shan--ide-add 'graphql-mode #'hydra-lsp/body)
+
+  (with-eval-after-load 'lsp-graphql
+    ;; redfine matching to not use gql server for non gql files
+    (defun lsp-graphql-activate-p (filename &optional _)
+      "Check if the GraphQL language server should be enabled based on FILENAME."
+      (string-match-p (rx (one-or-more anything) "."
+                          (or "graphql" "gql")eos)
+                      filename))))
