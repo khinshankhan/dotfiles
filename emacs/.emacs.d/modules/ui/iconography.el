236fbde2f3034cc0ff220007680323b0ad5ff068
Khinshan Khan <shan@uptogood.dev>
2023-09-19 17:23:46 -0400

    finally fix iconography

--- emacs/.emacs.d/modules/ui/iconography.el
+++ emacs/.emacs.d/modules/ui/iconography.el
@@ -1,8 +1,19 @@
 (require 'core-straight)
 
 (package! nerd-icons
+  :commands (nerd-icons-octicon
+             nerd-icons-faicon
+             nerd-icons-flicon
+             nerd-icons-wicon
+             nerd-icons-mdicon
+             nerd-icons-codicon
+             nerd-icons-devicon
+             nerd-icons-ipsicon
+             nerd-icons-pomicon
+             nerd-icons-powerline)
   :config
-  ;; TODO: figure out why this unless block isn't working
-  ;; (print (find-font (font-spec :name "Symbols Nerd Font Mono")))
-  (unless (find-font (font-spec :name "Symbols Nerd Font Mono"))
-    (nerd-icons-install-fonts t)))
+  (do-once-n-sec-after-emacs-startup!
+   0.1
+   (when (not (member "Symbols Nerd Font Mono" (font-family-list)))
+     (nerd-icons-install-fonts t)
+     (revert-buffer))))
