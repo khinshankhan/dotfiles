25dcff023b31ff73743815afabd11763efcbaf48
Khinshan Khan <shan@uptogood.dev>
2024-12-22 12:19:49 -0500

    remove problematic js tools

--- emacs/.emacs.d/modules/lang/js.el
+++ emacs/.emacs.d/modules/lang/js.el
@@ -1,33 +1,9 @@
 (require 'core-straight)
 
-;; tools
-(package! add-node-modules-path
-  :hook
-  ((html-mode
-    css-mode
-    web-mode
-    markdown-mode
-    js-mode
-    js2-mode
-    json-mode
-    rjsx-mode
-    typescript-mode
-    typescript-tsx-mode
-    solidity-mode) . add-node-modules-path))
-
-(package! prettier-js
-  :hook
-  ((html-mode
-    css-mode
-    web-mode
-    markdown-mode
-    js-mode
-    js2-mode
-    json-mode
-    rjsx-mode
-    typescript-mode
-    typescript-tsx-mode
-    solidity-mode) . prettier-js-mode))
+;; Ensure projectile is configured to handle JavaScript projects
+(with-eval-after-load 'projectile
+  (pushnew! projectile-project-root-files "package.json")
+  (pushnew! projectile-globally-ignored-directories "^node_modules$"))
 
 (after! yasnippet
   (package! react-snippets))
