b6823c922b6f7a58563a4179f8c488384bda3971
Khinshan Khan <shan@uptogood.dev>
2026-05-11 01:15:08 -0400

    tidy up python config

--- emacs/.emacs.d/modules/lang/python.el
+++ emacs/.emacs.d/modules/lang/python.el
@@ -1,19 +1,30 @@
 (require 'core-straight)
+(require 'core-module)
 
-;; sync with https://github.com/jchedal-anglay/emacs.d/blob/rewrite/modules/language/python.el
 (package! python
   :defer t
   :ensure nil
   :custom
   (python-indent 4)
-  (py-split-window-on-execute t))
+  (py-split-window-on-execute t)
+  :config
+  (when (and (equal python-shell-interpreter "python")
+             (executable-find "python3"))
+    (setq python-shell-interpreter "python3")
+    ;; Python 3.13+ workaround for pyrepl SIGINT mishandling under comint
+    (setenv "PYTHON_BASIC_REPL" "1"))
 
-;; Required for MacOS, prevents newlines from being displayed as ^G
-(setq python-shell-interpreter-args (if (equal system-type 'darwin)
-                                        "-c \"exec('__import__(\\'readline\\')')\" -i"
-                                      "-i"))
+  ;; Required for MacOS, prevents newlines from being displayed as ^G
+  (setq python-shell-interpreter-args (if (equal system-type 'darwin)
+                                          "-c \"exec('__import__(\\'readline\\')')\" -i"
+                                        "-i")))
 
-(defun igneous--activate-venv ()
+(with-eval-after-load 'projectile
+  (pushnew! projectile-project-root-files "pyproject.toml" "setup.py" "requirements.txt"))
+
+;; inspired by https://github.com/jchedal-anglay/emacs.d/blob/rewrite/modules/language/python.el
+(defun +python-activate-venv ()
+  "Prompt to activate a virtualenv if one isn't active."
   (interactive)
   (unless (bound-and-true-p pyvenv-virtual-env-name)
     (call-interactively #'pyvenv-activate)))
@@ -21,24 +32,21 @@
 (package! pyvenv
   :after python
   :hook
-  (python-mode . igneous--activate-venv)
+  (python-mode . +python-activate-venv)
   (python-mode . pyvenv-mode))
 
 (with-feature! +cython
   (package! cython-mode)
   (package! flycheck-cython
-    :if (module-p! :tools flycheck)
+    :if (core-module/feature-p :checkers 'syntax '+flycheck)
     :after flycheck
     :config
     (add-to-list 'flycheck-checkers 'cython)))
 
-(with-feature! +lsp
-  (if (module-p! :tools lsp)
-      (package! lsp-pyright
-        :init
-        (add-hook 'python-mode-hook #'lsp 80)  ;; 80 is the depth, helping making sure it runs after other hook functions
-        (require 'lsp-pyright))
-    (warn "Module language/python requires module tools/lsp for feature +lsp to work.")))
+(lsp! python-mode
+  (package! lsp-pyright
+    :after lsp-mode)
+  (auto-ide/add! 'python-mode #'hydra-lsp/body))
 
 (dap!
   (require 'dap-python))
