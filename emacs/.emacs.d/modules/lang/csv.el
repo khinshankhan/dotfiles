88e8280e6e8c564b2641e9f2ae05ee9e0d59fdd6
Khinshan Khan <shan@uptogood.dev>
2026-06-02 07:23:45 -0400

    disable lsp for csv/tsv

--- emacs/.emacs.d/modules/lang/csv.el
+++ emacs/.emacs.d/modules/lang/csv.el
@@ -1,5 +1,9 @@
 (require 'core-straight)
 
+(with-module! :tools lsp
+  (lsp-custom/ignore-mode 'csv-mode)
+  (lsp-custom/ignore-mode 'tsv-mode))
+
 (package! csv-mode
   :mode
   (("\\.csv\\'" . csv-mode)
